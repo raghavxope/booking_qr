48ff21d3d51aec84f074423d17f0410988c3e99c
raghavxope <akileshbabu1997@gmail.com>
2019-05-26 17:33:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/booking_qr/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.booking_qr;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.zxing.WriterException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner1,spinner2;
+    private Button btnSubmit;
+    private EditText c_no;
+    String TAG = "GenerateQRCode";
+    EditText edtValue;
+    ImageView qrImage;
+    String location,type;
+    Button start, save;
+    String inputValue;
+    String savePath = Environment.getExternalStorageDirectory().getPath() + "/QRCode/";
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addItemsOnSpinner2();
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
+        c_no=findViewById(R.id.car_no);
+        btnSubmit=findViewById(R.id.btnSubmit);
+    }
+    public void addItemsOnSpinner2() {
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        list.add("Sedan");
+        list.add("Hatchback");
+        list.add("SUV");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        qrImage = (ImageView) findViewById(R.id.QR_Image);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,
+                        "OnClickListener : " +
+                                "\nLocation : "+ String.valueOf(spinner1.getSelectedItem())+ "/nType :"+String.valueOf(spinner1.getSelectedItem())
+                        +"\nCar number:"+c_no.getText().toString()
+                               ,
+                        Toast.LENGTH_SHORT).show();
+                location=String.valueOf(spinner1.getSelectedItem());
+                type=String.valueOf(spinner1.getSelectedItem());
+
+                edtValue.setText("location"+"type"+c_no.getText());
+                inputValue = edtValue.toString().trim();
+                if (inputValue.length() > 0) {
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                    Display display = manager.getDefaultDisplay();
+                    Point point = new Point();
+                    display.getSize(point);
+                    int width = point.x;
+                    int height = point.y;
+                    int smallerDimension = width < height ? width : height;
+                    smallerDimension = smallerDimension * 3 / 4;
+
+                    qrgEncoder = new QRGEncoder(
+                            inputValue, null,
+                            QRGContents.Type.TEXT,
+                            smallerDimension);
+                    try {
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        qrImage.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        Log.v(TAG, e.toString());
+                    }
+                } else {
+                    edtValue.setError("Required");
+                }
+            }
+
+        });
+// this is for saving the qr code ,this is not being used anywhere 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean save;
+                String result;
+                try {
+                    save = QRGSaver.save(savePath, edtValue.getText().toString().trim(), bitmap, QRGContents.ImageType.IMAGE_JPEG);
+                    result = save ? "Image Saved" : "Image Not Saved";
+                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
